9f8c272b3df1b85fa745a0ec7ed64c2588758fc9
sahil1607cms <sahil16072004@gmail.com>
2025-01-15 23:20:19 +0530

    created bg color changer

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
-const [color,setColor]=useState('red');
 
 const App = () => {
+  const [color,setColor]=useState('green')
+
+  const style={
+    backgroundColor:{color}
+  }
   return (
-    <div className="wi-full h-screen ">
-      <div className=" flex justify-center gap-[30px] p-3 fixed bg-red-500 min-h-[50px] bottom-10 inset-x-0 shadow-2xl">
-        <button className="text-white rounded-md p-2 border-2">red</button>
-        <button onClick={setColor(green)}>green</button>
-          <div ></div>
+    <div className="w-screen h-screen " style={{backgroundColor:color}}>
+      <div className=" flex flex-wrap justify-center w-full p-3 fixed bg-white sm:min-h-[50px] min-h-[120px] bottom-10 shadow-2xl">
+        <div className='  flex flex-wrap item-center justify-center w-full max-w-2xl  gap-3 sm:h-12 h-28 shadow-lg rounded-3xl'>
+        <button className="px-3 rounded-3xl mt-1 min-w-[100px] p-2 border-2 h-10 bg-red-500 text-black"
+         onClick={()=>setColor('red')}>red</button>
+        <button className="px-3 rounded-3xl mt-1 min-w-[100px] w-[] p-2 border-2 h-10 bg-green-500 text-black"
+         onClick={()=>setColor('green')}>green</button>
+         <button className="px-3 rounded-3xl mt-1 min-w-[100px] w-[] p-2 border-2 h-10 bg-blue-500 text-black"
+         onClick={()=>setColor('blue')}>blue</button>
+        </div>
+
       </div>
       
     </div>
